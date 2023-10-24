9c5d2aad2a76c542eb5c3b553d764f7cf50fdf16
Ben <bparker@coldsnap.com>
2023-10-24 08:44:49 -0400

    Section 2, updated report.py to use list of dictionaries

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,31 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+
+if len(sys.argv)==2:
+    filename = sys.argv[2]
+else:
+    filename = 'Data/portfolio.csv'
+
+# def read_portfolio(filename):
+#     portfolio = []
+#     with open(filename, 'rt') as f:
+#         rows = csv.reader(f)
+#         headers = next(rows)
+#         for row in rows:
+#             portfolio.append((row[0], int(row[1]), float(row[2]), ))
+#     return portfolio
+
+def read_portfolio(filename):
+    portfolio = []
+    with open (filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            portfolio.append({'name': row[0],
+            'shares': row[1],
+            'price': row[2],})
+    return portfolio
