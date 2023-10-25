356fff56be2facbf314daefe7a02b93337eb3be6
Ben <bparker@coldsnap.com>
2023-10-25 06:37:34 -0400

    exercise 2.16, sequences and iteration

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,24 +4,25 @@
 import csv
 import sys
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
-
 
 def portfolio_cost(filename):
     total_cost = 0
     with open(filename, 'r') as f:
         rows = csv.reader(f)
         header = next(rows)
-        try:
-            for row in rows:
-                # stock = line.split(',')
-                total_cost += int(row[-2]) * float(row[-1])
-        except ValueError:
-            print('Oops:', row)
+        for i, row in enumerate(rows, start=1):
+            record = dict(zip(header, row))
+            try:
+                total_cost += int(record['shares']) * float(record['price'])
+            except ValueError:
+                print(f'Row {i}: Couldn"t convert: {row}') 
     return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
 
 cost = portfolio_cost(filename)
 print(f'Total cost ${cost:.2f}')  
