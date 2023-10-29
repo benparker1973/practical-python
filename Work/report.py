b29e73cc519f199f48b0117763d1ff0d11fa98f7
Ben <bparker@coldsnap.com>
2023-10-29 05:28:13 -0400

    exercise 3.2, modified to use a top level function

--- Work/report.py
+++ Work/report.py
@@ -5,21 +5,10 @@
 import csv
 import sys
 
-if len(sys.argv)==2:
-    filename = sys.argv[2]
-else:
-    filename = 'Data/portfolio.csv'
-
-# def read_portfolio(filename):
-#     portfolio = []
-#     with open(filename, 'rt') as f:
-#         rows = csv.reader(f)
-#         headers = next(rows)
-#         for row in rows:
-#             portfolio.append((row[0], int(row[1]), float(row[2]), ))
-#     return portfolio
 
 def read_portfolio(filename):
+    '''reads in a portfolio csv file
+    '''
     portfolio = []
     with open (filename, 'rt') as f:
         rows = csv.reader(f)
@@ -32,6 +21,8 @@ def read_portfolio(filename):
 
 
 def read_prices(filename):
+    '''reads in a csv file that has stock prices
+    '''
     prices = {}
     with open(filename, 'r') as f:
         rows = csv.reader(f)
@@ -44,6 +35,8 @@ def read_prices(filename):
 
 
 def gainloss(portfolio, price):
+    '''compares the portfolio cost to current prices and returns the gain or loss
+    '''
     total = 0
     for s in portfolio:
         total += s['shares'] * (price[s['name']] - s['price'])
@@ -51,6 +44,8 @@ def gainloss(portfolio, price):
 
 
 def make_report(portfolio, price):
+    '''generates a list of stocks in the portfolio
+    '''
     report = []
     for s in portfolio:
         chg = price[s['name']] - s['price']
@@ -58,12 +53,20 @@ def make_report(portfolio, price):
     return report
 
 def print_report(report):
+    '''prints a formatted report of stocks
+    '''
     header = ('Name', 'Shares','Price', 'Change',)
     print('{:>10s} {:>10s} {:>10s} {:>10s}'.format(*header))
     print('-'*45)
     for name, shares, price, change in report:
-        print(f'{name:>10s} {shares:>10d} ${price:>10.2f} {change:>10.2f}')
+        print(f'{name:>10s} {shares:>10d} {f"${price:.2f}":>10} {change:>10.2f}')
 
-prices = read_prices('Data/prices.csv')
-portfolio = read_portfolio(filename)
-report = make_report(portfolio, prices)
+
+def portfolio_report(portfolio_filename, price_filename):
+    prices = read_prices(price_filename)
+    portfolio = read_portfolio(portfolio_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
+
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
