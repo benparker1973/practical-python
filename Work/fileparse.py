c1cceec1b56cad3a618cc529ad315a737a105f6c
Ben <bparker@coldsnap.com>
2023-10-29 06:27:25 -0400

    added column selector, data type, and header option

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,18 +3,31 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     '''
     Parse a csv into a list of records
     '''
     with open(filename)as f:
         rows = csv.reader(f)
-        headers = next(rows)
+        if has_headers:
+            headers = next(rows)
+        if select:
+            indices = [headers.index(column) for column in select]
+            headers = select
+        else:
+            indices = []
         records = []
         for row in rows:
             if not row:
                 continue
-            record = (dict(zip(headers, row)))
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [type(val) for type, val in zip(types, row)]
+            if has_headers:
+                record = (dict(zip(headers, row)))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
