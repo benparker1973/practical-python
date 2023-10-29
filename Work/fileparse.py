2ba47c4b5ad5a352069a901920d153749696f252
Ben <bparker@coldsnap.com>
2023-10-29 05:35:19 -0400

    added parse_csv function

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,20 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename):
+    '''
+    Parse a csv into a list of records
+    '''
+    with open(filename)as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            record = (dict(zip(headers, row)))
+            records.append(record)
+
+    return records
